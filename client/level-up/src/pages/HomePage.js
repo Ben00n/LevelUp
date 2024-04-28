0dab4b2016e09c5150a8315e3010cf6efe5c5130
CombaticaBen00n <162020002+CombaticaBen00n@users.noreply.github.com>
2024-04-28 10:36:28 +0300

    removed auth from home page

--- client/level-up/src/pages/HomePage.js
+++ client/level-up/src/pages/HomePage.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import '../styles/HomePage.css';
 
 const HomePage = () => {
   return (
     <div className="home-page">
       <h1>Welcome to Level-Up</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-        </ul>
-      </nav>
     </div>
   );
 };
